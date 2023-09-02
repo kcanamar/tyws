ea0d12ace5d0fc263f7f071dfe112dc5afc03370
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-09-02 14:27:48 -0700

    footer

---
+++ src/utils/socials.js
@@ -0,0 +1,10 @@
+export const socials = {
+    facebook: "https://www.facebook.com/TheYardWorkShow",
+    twitter: "https://twitter.com/TheYardWorkShow",
+    instagram: "https://www.instagram.com/theyardworkshow/",
+    discord: "https://discord.com/invite/6RtX93jM8p",
+    tiktok: "https://www.tiktok.com/@theyardworkshow",
+    youtube: "https://www.youtube.com/channel/UCd2r61aFQ4NwdOsuaPJ_oxA",
+    spotify: "https://open.spotify.com/user/31wwtemf7lh6erzcusvbi5svc2ka?si=5fdef842d78e488b",
+    twitch: "https://www.twitch.tv/theyardworkshow"
+}
