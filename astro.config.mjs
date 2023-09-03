f2d26b5f50cf6ae20bd23f88e1f7738f6b77cc10
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-09-03 11:12:40 -0700

    contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
 import vercel from "@astrojs/vercel/serverless";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), markdoc()],
+  integrations: [tailwind(), markdoc(), react()],
   output: "server",
   adapter: vercel()
 });
